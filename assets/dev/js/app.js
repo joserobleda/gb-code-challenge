1bbbe914cb19c345f26221b8af53738553852376
Jose Ignacio Andres <joserobleda@gmail.com>
2014-02-12 19:01:48 +0100

    base js code

--- assets/dev/js/app.js
+++ assets/dev/js/app.js
@@ -4,6 +4,14 @@
 
 	var api = 'https://api.github.com';
 
+
+    var BasicView = Backbone.View.extend({
+        render: function () {
+            this.$el.html(this.template(this.model.attributes));
+            return this;
+        }
+    });
+
 	var User = Backbone.Model.extend({
 		urlRoot: api + '/users',
 
@@ -18,86 +26,151 @@
     });
 
 
-    var UserView = Backbone.View.extend({
+    var UserView = BasicView.extend({
  		el: $('#user'),
 
  		initialize: function () {
  			this.listenTo(this.model, "change", this.render);
-
  			this.model.fetch();
  		},
 
-        template: _.template($("#userview").html()),
-
- 		render: function () {
- 			this.$el.html(this.template(this.model.toJSON()));
- 		}
+        template: _.template($("#userview").html())
     });
 
-
-    var ListView = Backbone.View.extend({
-        el: $('#repos'),
-
-        initialize: function () {
-            this.listenTo(this.collection, "reset", this.render);
-            this.collection.fetch({reset: true});
-        },
-
-        render: function () {
-            console.log(this.collection);
-            this.$el.html('Found ' + this.collection.length + " repos");
-        }
-    });
 
 
     var Repository = Backbone.Model.extend({
 
+        initialize: function () {
+           
+        },
+
+        url: function () {
+            return api + '/repos/' + this.id;
+        }
     });
 
 
-	// Create a collection of services
+    var RepositoryView = BasicView.extend({
+        tagName: 'div',
+
+        id: 'repo',
+
+        template: _.template($("#repoview").html()),
+
+        initialize: function () {
+            this.listenTo(this.model, "change", this.render);
+            this.model.fetch();
+        }
+    });
+
+
+	
 	var Repositories = Backbone.Collection.extend({
 		model: Repository,
 
+        initialize: function (url) {
+            this.url = url;
+        },
+
+        // sort by star count and fork count from higher to lower values
+        comparator: function (a, b) {
+            if (a.get('stargazers_count') == b.get('stargazers_count')) {
+                return a.get('watchers_count') < b.get('watchers_count') ? 1 : -1;
+            }
+
+            return a.get('stargazers_count') < b.get('stargazers_count') ? 1 : -1;
+        },
 
         url: function () {
             return this.url;
-        },
-
-        initialize: function (url) {
-            this.url = url;
         }
 	});
 
+
+    var LiView = BasicView.extend({
+        tagName: 'li',
+
+        template: _.template($("#liview").html())
+    });
+
+
+    var ListView = Backbone.View.extend({
+        tagName: 'ul',
+
+        id: 'repos',
+
+        initialize: function () {
+            this.listenTo(this.collection, "reset", this.render);
+            this.collection.fetch({reset: true, error: this.error.bind(this)});
+
+            return this;
+        },
+
+
+        render: function () {
+            var view = this;
+
+            this.$el.empty();
+
+            this.collection.each(this.add.bind(this));
+            return this;
+        },
+
+
+        add: function (repository) {
+            var li = new LiView({
+                model: repository
+            });
+
+            li.render().$el.appendTo(this.$el);
+            return this;
+        },
+
+        error: function () {
+            this.$el.html("Whoops, we can't find the username in github. Do you write the username correctly?");
+        }
+    });
 
 
     var App = Backbone.View.extend({
 
         // Base the view on an existing element
-        el: $('#main'),
+        el: $('#content'),
 
 
-        initialize: function () {
-            
+        // Render the main/list/repo views based on the router params
+        render: function (username, repository) {
+            this.$el.empty().addClass('loading');
+
+            if (username) {
+                this.user = new UserView({
+                    model: new User({ id: username })
+                });
+
+                // draw a single repo or a list
+                if (repository) {
+                    this.view = new RepositoryView({
+                        model: new Repository({ id: username + '/' + repository })
+                    });
+
+                    this.view.model.on('change', this.show.bind(this));
+                } else {
+                    this.view = new ListView({
+                        collection: this.user.model.getRepositories()
+                    });
+
+                    this.view.collection.on('reset', this.show.bind(this));
+                }
+            } else {
+
+                if (this.user) this.user.$el.empty();
+            }
         },
 
-        setUser: function (username) {
-            var user, repositories;
-
-            user            = new User({id: username});
-            repositories    = user.getRepositories();
-
-            this.user = new UserView({
-                model: user
-            });
-
-            this.repositories = new ListView({
-                collection: repositories
-            });
-        },
-
-        hideUser: function () {
-            if (this.user) this.user.$el.empty();
+        // Show after ajax calls to minimize reflow/repaints
+        show: function () {
+            this.$el.removeClass('loading').html(this.view.$el);
         }
 
     });
@@ -108,33 +181,14 @@
     
     var Router = Backbone.Router.extend ({
         routes: {
-            '':             'home',
-            ':user':        'user',
-            ':user/:repo':  'repo'
-        },
-
-        home: function () {
-            app.hideUser();
-        },
-
-        user: function (username) {
-            app.setUser(username);
-        },
-
-        repo: function (username, repo) {
-            app.setUser(username);
+            '':             app.render.bind(app),
+            ':user':        app.render.bind(app),
+            ':user/:repo':  app.render.bind(app)
         }
     });
 
-
-    //define our new instance of router
-    var router = new Router();
-     
-    // use html5 History API
+    
+    new Router();
     Backbone.history.start();
-
-    router.navigate('/joserobleda'); 
-
-
 
 }());
