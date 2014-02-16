2361998cad256c9cfb0745b7cd7880f914c2b4ae
Jose Ignacio Andres <joserobleda@gmail.com>
2014-02-16 23:43:10 +0100

    add moment to jasmine

--- Gruntfile.js
+++ Gruntfile.js
@@ -3,7 +3,7 @@ module.exports = function(grunt) {
 	grunt.initConfig({
 		jasmine: {
 			pivotal: {
-				src: ['assets/dev/js/jquery.js', 'assets/dev/js/underscore.js', 'assets/dev/js/backbone.js', 'assets/dev/js/app.js'],
+				src: ['assets/dev/js/jquery.js', 'assets/dev/js/underscore.js', 'assets/dev/js/backbone.js', 'assets/dev/js/moment.js', 'assets/dev/js/app.js'],
 				options: {
 					specs: 'test/spec/*.js',
 					helpers: 'test/lib/sinon-1.8.2.js'
