37c8b29ece806b02a0403956308640423a7f9810
Jose Ignacio Andres <joserobleda@gmail.com>
2014-02-16 10:11:01 +0100

    test idAttribute

--- test/spec/Repository.js
+++ test/spec/Repository.js
@@ -3,4 +3,8 @@ describe("Respository Model", function() {
 	it("should exist", function() {
 		expect(App.Models.Repository).toBeDefined()
 	});
+
+	it("the id attribute should be idAttribute", function () {
+		expect(App.Models.Repository.prototype.idAttribute).toBe("full_name");
+	});
 });
